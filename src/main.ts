493be9270d188fd7fef57564bacd3e942caee034
Agustin Gabrielli <agabrielli@fi.uba.ar>
2026-03-09 23:47:01 -0300

    feat(03-03): wire GenerationService into main.ts
    - Import GenerationService from ./generation and Notice from obsidian
    - Create status bar item in onload() and pass to GenerationService constructor
    - Register 'generate-self-test' command that resolves folder from active file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
 import { ActiveRecallSettings, DEFAULT_SETTINGS, ActiveRecallSettingTab } from './settings';
+import { GenerationService } from './generation';
 
 export default class ActiveRecallPlugin extends Plugin {
     settings: ActiveRecallSettings;
@@ -7,6 +8,24 @@ export default class ActiveRecallPlugin extends Plugin {
     async onload() {
         await this.loadSettings();
         this.addSettingTab(new ActiveRecallSettingTab(this.app, this));
+
+        const statusBarItem = this.addStatusBarItem();
+        statusBarItem.setText('');
+        const generationService = new GenerationService(this.app, this.settings, statusBarItem);
+
+        this.addCommand({
+            id: 'generate-self-test',
+            name: 'Generate Self-Test for Current Folder',
+            callback: async () => {
+                const activeFile = this.app.workspace.getActiveFile();
+                if (!activeFile) {
+                    new Notice('Open a file inside the folder you want to generate a self-test for.');
+                    return;
+                }
+                const folderPath = activeFile.parent?.path ?? '/';
+                await generationService.generate(folderPath);
+            },
+        });
     }
 
     onunload() {}
